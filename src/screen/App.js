db83f23950f904b3e26eede03f6496db342bf2e8
Donghee Lee <jacobdhlee@gmail.com>
2018-09-03 14:22:11 -1000

    done

---
+++ src/screen/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Body, Main, MainText } from "./style";
+
+class App extends Component {
+  render() {
+    return (
+      <Body>
+        <Main>
+          <MainText>
+            We are currently working on launching!
+          </MainText>
+          <MainText>See you soon!</MainText>
+        </Main>
+      </Body>
+    );
+  }
+}
+
+export default App;
---
+++ src/screen/style.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import backgroundPic from "../pics/pics.jpg";
+
+const Body = styled.div`
+  background-color: #fdfdfd
+  margin: 0
+`;
+const Main = styled.div`
+  display: flex
+  flex-direction: column
+  height: 100vh
+  width: 100vw
+  justify-content: center
+  align-items: center
+  background: url(${backgroundPic}) no-repeat center center
+  background-size: cover
+`;
+
+const MainText = styled.h1`
+  font-size: 3vw;
+  font-weight: bold
+  color: white
+`;
+
+export { Body, Main, MainText };
